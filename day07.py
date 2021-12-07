fbcba137ac8cf5c5c262feb746782aaffe908e57
Arnau Quindós <aquindsa10@alumnes.ub.edu>
2021-12-07 09:46:59 +0100

    Day 7

--- day07.py
+++ day07.py
@@ -8,15 +8,12 @@ def part2(data):
     return np.min([np.sum([get_cost(y) for y in np.abs(data-x)]) for x in range(data.min(), data.max())])
 
 def get_cost(n):
-    if n not in d:
-        d[n] = int(np.sum(range(1,n+1)))
-    return d[n]
+    return n*(n+1)/2
 
 if __name__ == "__main__":
     day = 7
     data = get_input(day, splitlines=False).split(',')
     data = np.array([int(x) for x in data])
-    d = {}
-    print(part1(data))
-    print(part2(data))
+    print(int(part1(data)))
+    print(int(part2(data)))
 
