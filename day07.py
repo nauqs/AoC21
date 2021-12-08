ba4d56ea01a3636b4b2610b1e40ffab7e2ddc24e
Arnau Quindós <aquindsa10@alumnes.ub.edu>
2021-12-08 06:44:42 +0100

    Revert "Day 7 update"
    This reverts commit 91b5aeb9caf89d6d76b76e594adde9cfd8b600b0.

--- day07.py
+++ day07.py
@@ -5,7 +5,10 @@ def part1(data):
     return np.min([np.sum(np.abs(data-x)) for x in range(data.min(), data.max())])
 
 def part2(data):
-    return np.min([np.sum((np.abs(data-x)*(np.abs(data-x)+1)/2)) for x in range(data.min(), data.max())])
+    return np.min([np.sum([get_cost(y) for y in np.abs(data-x)]) for x in range(data.min(), data.max())])
+
+def get_cost(n):
+    return n*(n+1)/2
 
 if __name__ == "__main__":
     day = 7
